b500a595b38036ab16a71a2846d9cfdde526e098
Mir Faris <mirfaris79@gmail.com>
2024-05-10 19:42:10 +0600

    Separate style for login screen

--- lib/screens/onBoarding/login_screen.dart
+++ lib/screens/onBoarding/login_screen.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:task_manager/styles/styles.dart';
@@ -11,7 +10,6 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-
   bool _isObscure = true;
 
   TextEditingController _EmailEditingController = TextEditingController();
@@ -24,89 +22,55 @@ class _LoginScreenState extends State<LoginScreen> {
         children: [
           ScreenBackground(context),
           Container(
-            padding: EdgeInsets.symmetric(vertical: 30, horizontal: 90),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                SvgPicture.asset("assets/images/logo.svg",
-                  height: 180,
-                  width: 180,
-
-                ),
-
-                const SizedBox(height: 30,),
-
-
-                Text("Get started",
-                 style:  TextStyle(
-                  fontFamily: "poppins",
-                   fontSize: 18, // Adjust font size as needed
-                   fontWeight: FontWeight.w700, // Adjust font weight as needed
-                   color: Colors.black,
-
-
-
-                ),),
-
-                const SizedBox(height: 8,),
-
-
-                TextFormField(  controller: _EmailEditingController,
-                  style: TextStyle(fontSize: 18,),
-                  decoration: InputDecoration(
-                      labelText: "Enter your name:",
-                      hintText: "John Doe",
-                      filled: true,
-                      fillColor: Colors.grey.shade200,
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.teal, width: 1)
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.grey.shade200, width: 0.0)
-                    ),
-
-
+              padding: EdgeInsets.symmetric(vertical: 30, horizontal: 90),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SvgPicture.asset(
+                    "assets/images/logo.svg",
+                    height: 180,
+                    width: 180,
                   ),
-                ),
-
-                const SizedBox(height: 8,),
-
-
-
-            TextFormField(
-            controller: _PassTextEditingController,
-            style: TextStyle(fontSize: 18),
-            obscureText: _isObscure,
-            decoration: InputDecoration(
-              labelText: "Enter password:",
-              filled: true,
-              fillColor: Colors.grey.shade200,
-              focusedBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.teal, width: 1),
-              ),
-              enabledBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.grey.shade200, width: 0.0),
-              ),
-              suffixIcon: IconButton(
-                icon: Icon(_isObscure ? Icons.visibility_off : Icons.visibility),
-                onPressed: () {
-                  setState(() {
-                    _isObscure = !_isObscure;
-                  });
-                },
-              ),
-            ),
-          )
-
-
-
-
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 200.0),
+                    child: Text("Get started", style: HeadText1(lightTeal)),
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  TextFormField(
+                      controller: _EmailEditingController,
+                      style: const TextStyle(
+                        fontSize: 18,
+                      ),
+                      decoration: textField("Enter your name", "John Doe")),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  TextFormField(
+                      controller: _PassTextEditingController,
+                      style: const TextStyle(fontSize: 18),
+                      obscureText: _isObscure,
+                      decoration: passwordField(
+                        "Enter password",
+                        IconButton(
+                          icon: Icon(_isObscure
+                              ? Icons.visibility_off
+                              : Icons.visibility),
+                          onPressed: () {
+                            setState(() {
+                              _isObscure = !_isObscure;
+                            });
+                          },
+                        ),
+                      )),
+                ],
+              )),
         ],
-            )
-          ),
-        ],
-
       ),
     );
   }
--- lib/styles/styles.dart
+++ lib/styles/styles.dart
@@ -1,14 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-SvgPicture ScreenBackground(context){
+const lightTeal = Color.fromRGBO(15, 147, 122, 0.8);
 
-  return SvgPicture.asset("assets/images/background.svg",
-
+SvgPicture ScreenBackground(context) {
+  return SvgPicture.asset(
+    "assets/images/background.svg",
     width: double.infinity,
     height: double.infinity,
     fit: BoxFit.cover,
     alignment: Alignment.center,
   );
+}
 
+TextStyle HeadText1(textColor) {
+  return TextStyle(
+    fontFamily: "poppins",
+    fontSize: 20, // Adjust font size as needed
+    fontWeight: FontWeight.bold, // Adjust font weight as needed
+    color: textColor,
+  );
+}
+
+InputDecoration textField(label, hintText) {
+  return InputDecoration(
+    labelText: label,
+    hintText: hintText,
+    filled: true,
+    fillColor: Colors.grey.shade200,
+    focusedBorder: const OutlineInputBorder(
+        borderSide: BorderSide(color: Colors.teal, width: 1)),
+    enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: Colors.grey.shade200, width: 0.0)),
+  );
+}
+
+InputDecoration passwordField(label, showPass) {
+  return InputDecoration(
+    labelText: label,
+    filled: true,
+    fillColor: Colors.grey.shade200,
+    focusedBorder: const OutlineInputBorder(
+      borderSide: BorderSide(color: Colors.teal, width: 1),
+    ),
+    enabledBorder: OutlineInputBorder(
+      borderSide: BorderSide(color: Colors.grey.shade200, width: 0.0),
+    ),
+    suffixIcon: showPass,
+  );
 }
